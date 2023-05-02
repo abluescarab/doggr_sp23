fe3884ab10e0f4801855844a38249fdcee8fae99
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-02 15:25:37 -0700

    Added frontend

---
+++ frontend/src/components/Banner.tsx
@@ -0,0 +1,14 @@
+// Note we MUST export this interface for TypeDoc to catch it!
+export interface BannerProps {
+  /**
+   * Is this component disabled?
+   */
+  disabled?: boolean;
+}
+
+/**
+ * Banner button that shows static "BannerLabel" text and does nothing
+ */
+export const Banner = ({ disabled = false }: BannerProps) => {
+  return <button disabled={disabled}>{"BannerLabel"}</button>;
+};
