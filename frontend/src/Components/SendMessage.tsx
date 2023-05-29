59cfee29a8abde8f7830d3126e1673dd5a7afacd
Alana Gilston <abluescarab@users.noreply.github.com>
2023-05-28 18:58:25 -0700

    Allow sending messages

--- frontend/src/Components/SendMessage.tsx
+++ frontend/src/Components/SendMessage.tsx
@@ -1,9 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { httpClient } from "@/Services/HttpClient.tsx";
+import { useAuth } from "@/Services/Auth.tsx";
 
 const SendMessage = () => {
   const [profile, setProfile] = useState(null);
+  const [responseText, setResponseText] = useState("");
   const location = useLocation();
+  const auth = useAuth();
+
+  const message = useRef<HTMLInputElement>(null);
+  const responseBox = useRef<HTMLParagraphElement>(null);
 
   useEffect(() => {
     const getProfile = () => {
@@ -13,35 +20,54 @@ const SendMessage = () => {
     getProfile();
   }, [location.state.profile]);
 
-  const sendMessage = () => {
-    // TODO: send message
+  const sendMessage = (e) => {
+    e.preventDefault();
+    responseBox.current.classList.remove("hidden");
+
+    if (auth.userId === profile.id) {
+      setResponseText("You cannot send a message to yourself.");
+      return;
+    }
+
+    httpClient
+      .post("/messages", {
+        sender_id: auth.userId,
+        receiver_id: profile.id,
+        message: message.current.value,
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          setResponseText("Message sent.");
+        } else {
+          setResponseText(`Failed to send message: ${response.statusText}`);
+        }
+      });
   };
 
   if (profile) {
     return (
       <div
-        className={
-          "flex flex-row items-center rounded-box bg-slate-700 w-4/5 mx-auto p-5"
-        }>
-        <div className={"flex flex-row items-center w-2/12"}>
+        className={"flex flex-col items-center bg-slate-700 w-4/5 mx-auto p-5"}>
+        <div className={"flex flex-1 flex-row items-center w-full"}>
           <img
-            className="rounded w-128 h-128 w-6/12"
+            className="rounded w-128 h-128 w-1/12"
             src={profile.imgUri}
             alt="Profile of pet"
           />
-          <p className={"font-bold pl-5 w-6/12"}>{profile.name}</p>
-        </div>
-        <div className={"flex-1"}>
+          <p className={"font-bold pl-5 w-1/12"}>{profile.name}</p>
           <input
-            className={"p-3 w-11/12 border-0 rounded-box"}
+            className={"p-3 w-9/12 border-0 rounded-box mx-5"}
             type={"text"}
             placeholder={"Enter your message here..."}
+            ref={message}
           />
-        </div>
-        <div className={"w-1/12"}>
           <button className="btn btn-circle" onClick={sendMessage}>
             Send
           </button>
+        </div>
+        <div className={"w-100 p-0 m-0 hidden"} ref={responseBox}>
+          <p>{responseText}</p>
         </div>
       </div>
     );
